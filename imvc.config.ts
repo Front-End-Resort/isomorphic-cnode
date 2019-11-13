52251dbd7fff801ffd9ad965267238af3ac11a67
tqma <tqma@ctrip.com>
2019-11-13 16:24:06 +0800

    update react-imvc to 3.0.0-beta.7

--- imvc.config.ts
+++ imvc.config.ts
@@ -1,8 +1,11 @@
-var path = require('path')
+import path from 'path'
+import { Config } from 'react-imvc'
 
-module.exports = {
+const config: Config = {
 	restapi: 'https://cnodejs.org/api/v1',
 	favicon: path.join(__dirname, 'favicon.ico'),
 	staticEntry: 'index.html',
 	// bundleAnalyzer: true,
 }
+
+export default config
